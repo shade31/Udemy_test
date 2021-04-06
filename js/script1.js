7cdfd903f67f741c7d1a128fe770ca4600d4fbcd
Ansar <Shade25626@gmail.com>
2021-04-06 21:02:00 +0600

    usloviya

---
+++ js/script1.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+
+const num = 51;
+
+switch (num) {
+    case 49: console.log('neverno'); break;
+    case 100: console.log('neverno'); break;
+    case 50: console.log('verno!'); break;
+    default: console.log('ne v etot raz'); break;
+}
