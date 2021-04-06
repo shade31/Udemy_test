bbd00ae046a5b113d67b35d2c35103e775cfc54c
Ansar <Shade25626@gmail.com>
2021-04-06 20:40:32 +0600

    ch1. Nachinaem sozdavat' prilojeniya

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцениваете?",""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцениваете?","");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
