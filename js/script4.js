95d273fab4d4d88dae3c42479203da2cd29cb427
Ansar <example@gmail.com>
2021-04-07 14:12:51 +0600

    ch2.dopolnenie

--- js/script4.js
+++ js/script4.js
@@ -11,18 +11,31 @@ const personalMovieDB = {
 };
 
 
-for (let i=0; i < 2; i++) {
+// for (let i=0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//           b = prompt("На сколько оцениваете?","");
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b; 
+//         console.log('done!');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+let i = 0;
+
+// while (i < 4) {
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//           b = prompt("На сколько оцениваете?","");
+//     i++;
+// }
+
+do {
     const a = prompt("Один из последних просмотренных фильмов", ""),
           b = prompt("На сколько оцениваете?","");
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b; 
-        console.log('done!');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
+    i++;
+} while (i < 4);
 
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
