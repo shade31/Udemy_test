de040a2d9f719e3751f45695456b4388d47c2a55
Ansar <example@gmail.com>
2021-04-07 13:43:07 +0600

    cikly

---
+++ js/script2.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+do {
+    console.log(num);
+    num++;
+} 
+while (num < 55);
+
+let num = 50;
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue
+    }
+    console.log(i);
+}
