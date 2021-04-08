5e3d96481736364662b779fc2c23c18ba9def017
Ansar <example@gmail.com>
2021-04-08 13:46:27 +0600

    metody

---
+++ js/script6.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const str = 'test';
+const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world!";
+
+// console.log(logg.slice(4));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
