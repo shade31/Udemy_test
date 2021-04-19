79651e04c3724efebd5733c3f3f63029aafc1154
Ansar <example@gmail.com>
2021-04-19 14:04:32 +0600

    zadachi sobesedovanii

---
+++ js/script14.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let x = 5; alert( x++ );
+
+// [] + false - null + true;
+// console.log([] + false - null + true); //NaN
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// console.log([] + 1 + 2);
+
+// alert ("12"[1]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И(&&) запинается на лжи
+// ИЛИ(||) запинается на правде
+
+// console.log(!!(1 && 2) == (1 && 2));
+
+// alert (null || 2 && 3 || 4);
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3];
+
+// console.log(a == b);
+
+// alert (+"Infinity");
+
+// console.log("Ежик" > "яблоко");
+
+console.log(0 || "" || 2 || undefined || true || false); //2
