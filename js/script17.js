faeca768762ecd679d88bee20a2e9da01783fbba
Ansar <example@gmail.com>
2021-04-20 14:21:49 +0600

    otrabotka deistvii v2

--- js/script17.js
+++ js/script17.js
@@ -1,12 +1,41 @@
 "use strict";
 
-const reklama = document.querySelector('.promo__adv'),
-      genre = document.querySelector('.promo__genre'),
-      bg = document.querySelector('.promo__content');
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const reklama = document.querySelectorAll('.promo__adv img'),
+      bg = document.querySelector('.promo__bg'),     
+      genre = bg.querySelector('.promo__genre'),
+      films = document.querySelector('.promo__interactive-list');
 
 
-reklama.remove();
+reklama.forEach(item => {
+    item.remove();
+});
+
+// reklama.remove(); //owibka
 
 genre.textContent = "Драма";
 
-bg.style.cssText = `background: url(img/bg.png)`
+bg.style.backgroundImage = 'url("img/bg.jpg")';
+
+films.innerHTML = "";
+
+movieDB.movies.sort();
+
+console.log(bg.innerHTML);
+
+movieDB.movies.forEach((film, i) => {
+    films.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
