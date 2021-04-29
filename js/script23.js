548ecb1ae218602d49e8fc1638df28d3733bea74
example <test@example.com>
2021-04-29 14:20:01 +0600

    scripty

---
+++ js/script23.js
@@ -0,0 +1,39 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
+
+    tabsParent.addEventListener('click', (e) => {
+        cont target = e.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
