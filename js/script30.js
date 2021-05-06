141fea63fd31eaceb95ab295bf56ef433a38b51a
example <test@example.com>
2021-05-06 14:42:33 +0600

    kontekst vyzova. this

---
+++ js/script30.js
@@ -0,0 +1,74 @@
+"use strict";
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+
+//     console.log(sum());
+// }
+
+// showThis(4, 5);
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+
+// obj.sum();
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(13));
+
+
+//1) Обычная фунция: this = window, но если стоит use strict - undefined
+//2) Контекст у методов объета - сам объект
+//3) this в конструкторах и классах - это новый экземпляр объекта
+//4) Ручная привязка this: call, apply, bind
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'red';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num);
+        };
+
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = a => a * 2;
+
+console.log(double(4));
